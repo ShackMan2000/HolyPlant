7ead853910c3c37ac09e7799ea2f275eeceb92ad
eioe <felix_klotzsche@web.de>
2020-02-08 15:41:49 +0100

    tryouts plant

---
+++ HolyPlant/Assets/Scripts/ManagePlant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagePlant : MonoBehaviour
+{
+    public GameObject PlantPart;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {  
+            GameObject NewPPart = Instantiate(PlantPart);
+        }
+    }
+
+
+}
