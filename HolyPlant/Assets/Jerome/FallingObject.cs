d65f5a89e281498724b9f5ba70e8fab27e2130db
juruhn <jeroen@the1stfloor.net>
2020-02-08 15:55:10 +0100

    no message

---
+++ HolyPlant/Assets/Jerome/FallingObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    public void Init (FallingObjectData objectSettings) {
+        // Set objecttype
+        Debug.Log ("I am a " + objectSettings.getMass());
+    }
+
+}
