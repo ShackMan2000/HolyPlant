38ffad9ad4872535a7e40d6b4b8a1c5daca1cbb0
ShackMan2000 <julianpieper@gmx.de>
2020-02-08 16:35:22 +0100

    no message

---
+++ HolyPlant/Assets/Scripts/Plant.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField]
+    private float growStagePerMinute;
+
+
+    public float growStage;
+
+
+    [SerializeField]
+    private List<GameObject> plantParts;
+
+
+
+    public void NewGame()
+    {
+        growStage = 0f;
+    }
+
+
+
+
+    
+
+
+
+
+
+    private void SetSprites()
+    {
+        int stages = Mathf.CeilToInt(growStage);
+
+        foreach (var part in plantParts)
+        {
+            part.gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < stages; i++)
+        {
+            plantParts[i].gameObject.SetActive(true);
+        }        
+
+    }
+
+
+
+
+  
+}
